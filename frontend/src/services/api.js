0ed758cb8804018eca72e043b07ac11dbe453da1
Satyam Singh <satyamsingh1902006@gmail.com>
2025-11-12 00:25:02 +0530

    Added API routes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const API_URL = process.env.VITE_API_URL
 const API = axios.create({
-  baseURL: `${process.env.VITE_API_URL}/api`,
+  baseURL: `${API_URL}/api`,
   withCredentials: true,
 });
 
