56745b27e1038b16d59a8cfd3520222b93770ba9
Satyam Singh <satyamsingh1902006@gmail.com>
2025-11-11 22:08:28 +0530

    port fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,5 +19,5 @@ app.get("/", (req, res) => {
   res.send("API is running ✅");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`✅ Server running at http://localhost:${PORT}`));
