4b52d48c5e9cc40bbb314c5361281ebe6a12e412
Satyam Singh <satyamsingh1902006@gmail.com>
2025-11-26 23:27:59 +0530

    fixed2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LightRays from "../components/LightRays";
-import "./home.css";
+import "./Home.css";
 
 export default function Home() {
   return (
